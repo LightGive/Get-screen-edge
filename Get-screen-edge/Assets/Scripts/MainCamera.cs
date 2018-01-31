afd5ab49b2dc5fd79c1d5ffea242b41af69705e3
LightGive <roi.akase@outlook.jp>
2018-01-31 20:29:49 +0900

    プレイヤーを画面外に出ないように変更

---
+++ Get-screen-edge/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour 
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    /// <summary>
+    /// 右端
+    /// </summary>
+    /// <value>The right.</value>
+    public float Right  { get { return getScreenBottomRight().x; } }
+    /// <summary>
+    /// 左端
+    /// </summary>
+    /// <value>The left.</value>
+    public float Left   { get { return getScreenTopLeft().x; } }
+    /// <summary>
+    /// 上端
+    /// </summary>
+    /// <value>The top.</value>
+    public float Top    { get { return getScreenTopLeft().y; } }
+    /// <summary>
+    /// 下端
+    /// </summary>
+    /// <value>The bottom.</value>
+    public float Bottom { get { return getScreenBottomRight().y; } }
+
+    void Start()
+    {
+        // カメラオブジェクトを取得します
+        GameObject obj = GameObject.Find("Main Camera");
+        mainCamera = obj.GetComponent<Camera>();
+
+        // 座標値を出力
+        Debug.Log("右端" + Right.ToString());
+        Debug.Log("左端" + Left.ToString());
+
+    }
+
+    /// <summary>
+    /// 画面の左上をの座標を取得
+    /// </summary>
+    /// <returns>The screen top left.</returns>
+    private Vector3 getScreenTopLeft()
+    {
+        Vector3 topLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
+        topLeft.Scale(new Vector3(1f, -1f, 1f));
+        return topLeft;
+    }
+
+    /// <summary>
+    /// 画面の右下の座標を取得
+    /// </summary>
+    /// <returns>The screen bottom right.</returns>
+    private Vector3 getScreenBottomRight()
+    {
+        Vector3 bottomRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
+        bottomRight.Scale(new Vector3(1f, -1f, 1f));
+        return bottomRight;
+    }
+}
---
+++ Get-screen-edge/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private MainCamera mainCamera;
+    [SerializeField]
+    private float speed;
+
+    /// <summary>
+    /// Spriteの幅を取得
+    /// </summary>
+    /// <value>The width of the sprite.</value>
+    public float SpriteWidth
+    {
+        get
+        {
+            var spr = gameObject.GetComponent<SpriteRenderer>();
+            return spr.sprite.bounds.size.x;
+        }
+    }
+
+    /// <summary>
+    /// Spriteの高さを取得
+    /// </summary>
+    /// <value>The height of the sprite.</value>
+    public float SpriteHeight
+    {
+        get
+        {
+            var spr = gameObject.GetComponent<SpriteRenderer>();
+            return spr.sprite.bounds.size.y;        
+        }
+    }
+
+    private void Update()
+    {
+        var vec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        var movePos = transform.position + vec * speed * Time.deltaTime;
+        transform.position = ClampPosition(movePos);
+    }
+
+    /// <summary>
+    /// 座標に制限をかける
+    /// </summary>
+    /// <returns>The position.</returns>
+    /// <param name="_pos">Position.</param>
+    Vector3 ClampPosition(Vector3 _pos)
+    {
+        var x = Mathf.Clamp(_pos.x, mainCamera.Left + (SpriteWidth / 2.0f), mainCamera.Right - (SpriteWidth / 2.0f));
+        var y = Mathf.Clamp(_pos.y, mainCamera.Bottom + (SpriteHeight / 2.0f), mainCamera.Top - (SpriteHeight / 2.0f));
+        return new Vector3(x, y, 0);
+    }
+}
